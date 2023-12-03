6cd7b765889598f6f4c9015e371f7a637a29a03b
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-12-03 21:01:19 +0200

    0.2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 def print_list_integer(my_list=[]):
     counter = 0
     while (counter < len(my_list)):
-        print(my_list[counter])
+        print("{:d}".format(my_list[counter]))
         counter += 1
