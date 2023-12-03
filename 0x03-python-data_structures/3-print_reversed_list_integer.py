f2e521347bdcb8522aeeecbae717643d9d67175b
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2023-12-03 21:31:05 +0200

    3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    counter = len(my_list)
+    while (counter):
+        print("{:d}".format(my_list[counter - 1]))
+        counter -= 1
