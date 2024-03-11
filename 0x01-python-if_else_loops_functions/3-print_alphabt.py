aa7f892da3eb32e0f425ab5229b0a71cdfa86e49
Ahmed-Nageb <115940327+Ahmed-Nageb@users.noreply.github.com>
2024-03-11 14:49:32 +0200

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Print the alphabet in lowercase, not followed by a new line."""
-
-for char in range(97, 123):
-    if chr(char) == 'q' or chr(char) == 'e':
-        continue
-    else:
-        print("{}".format(chr(char)), end="")
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-"""Print numbers 0 to 98 in decimal and hexadecimal."""
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
-    else:
-        print("{:02}".format(number), end=", ")
