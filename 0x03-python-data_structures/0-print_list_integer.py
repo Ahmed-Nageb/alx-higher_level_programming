93558eda3c928b3af7301fea9f854c3c97dcb869
Ahmed-Nageb <115940327+Ahmed-Nageb@users.noreply.github.com>
2024-03-11 14:49:57 +0200

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/0-print_list_integer.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    counter = 0
-    while (counter < len(my_list)):
-        print("{:d}".format(my_list[counter]))
-        counter += 1
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        counter = len(my_list)
-        while (counter):
-            print("{:d}".format(my_list[counter - 1]))
-            counter -= 1
