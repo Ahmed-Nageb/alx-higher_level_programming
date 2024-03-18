265d206e6de79571145714efd6baa44468735224
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-03-19 00:28:57 +0200

    10.1

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     if len(my_list) == 0:
-        return "None"
+        return my_list
     else:
         for num in my_list:
             if num % 2 == 0:
