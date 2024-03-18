790044cd2db22569569e559788e501e5c29cb4bf
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-03-18 23:01:06 +0200

    5.1

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_list = []
-    iter = 0
-    for iter in range(len(my_string)):
-        if my_string[iter] not in 'cC':
-            new_list.append(my_string[iter])
-    return new_list
+    new_string = my_string.translate({ord(iter): None for iter in 'cC'})
+    return new_string
