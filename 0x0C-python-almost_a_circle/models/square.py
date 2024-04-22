3d9d69b35925c9d2a9141412d02aecdaa1e16113
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-04-23 01:08:20 +0200

    12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -36,3 +36,20 @@ class Square(Rectangle):
     def __str__(self):
         """Returns a string representation of the square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """Updates instance attributes"""
+
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+            """9"""
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
