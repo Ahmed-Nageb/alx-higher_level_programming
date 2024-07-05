3360c875e7d92ef239d60ac663a8c62febc54f70
Ahmed-Nageb <ahmednageb61.an@gmail.com>
2024-07-06 00:41:19 +0300

    pycode

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     """fetches https://alx-intranet.hbtn.io/status using requests"""
-    url ='https://alx-intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
